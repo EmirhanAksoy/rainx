460e2f966895e5ff9c5432084d852537fccb3434
EmirhanAksoy <emirhan.aksoy@outlook.com.tr>
2020-11-17 22:24:34 +0300

    Styles updated.

--- projects/rainx-upload/src/lib/rainx-upload.component.ts
+++ projects/rainx-upload/src/lib/rainx-upload.component.ts
@@ -8,18 +8,22 @@ import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild }
 export class RainxUploadComponent {
 
   @ViewChild('hiddenInput') public someInput: any | undefined;
+
   @Output() public uploadFinished = new EventEmitter<any>(true);
+
   @Input() public files: File[] = [];
   @Input() public multiple = false;
   @Input() public class = 'btn btn-sm w-100 btn-primary';
   @Input() public accept = '*';
   @Input() public text = 'Upload file';
+  @Input() public showFileList = false;
 
   constructor() { }
 
-  public change(event: any): void {
-    this.files = event.target.files;
-    this.uploadFinished.emit(this.files);
+
+
+  public get filesExist(): boolean {
+    return this.files && this.files.length > 0;
   }
 
   public get fileName(): string {
@@ -27,14 +31,29 @@ export class RainxUploadComponent {
       return `${this.files.length} files`;
     }
     if (this.files.length === 1) {
-      const fileName = this.files[0].name;
-      return `${fileName.substring(0, 15)} ...  .${this.files[0].name.split('.').pop()}`;
+      const fileName = this.shortFileName(this.files[0]);
+      return fileName;
     }
     return this.text;
   }
 
   public uploadClick(event: any): void {
     this.someInput.nativeElement.click();
+  }
+
+  public change(event: any): void {
+    this.files = Array.from(event.target.files);
+    this.uploadFinished.emit(this.files);
+  }
+
+  public removeFile(event: any, file: File): void {
+    const index = this.files.map(x => x.lastModified).indexOf(file.lastModified);
+    this.files.splice(index, 1);
+    this.uploadFinished.emit(this.files);
+  }
+
+  public shortFileName(file: File): string {
+    return `${file.name.substring(0, 10)} ...  .${file.name.split('.').pop()}`;
   }
 
 }
