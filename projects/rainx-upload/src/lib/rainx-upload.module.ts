e19a93d31abfb97b663b6ea08fa0c267c18aac99
EmirhanAksoy <emirhan.aksoy@outlook.com.tr>
2020-11-22 17:39:26 +0300

    removed unnecessary imports.

--- projects/rainx-upload/src/lib/rainx-upload.module.ts
+++ projects/rainx-upload/src/lib/rainx-upload.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RainxUploadComponent } from './rainx-upload.component';
 
 
