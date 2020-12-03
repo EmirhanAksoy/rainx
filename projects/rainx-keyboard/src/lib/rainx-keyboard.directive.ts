537385e913838b9a4257a87c9bb7b0c565ef9778
EmirhanAksoy <emirhan.aksoy@outlook.com.tr>
2020-12-03 22:10:04 +0300

    Refactoring.

--- projects/rainx-keyboard/src/lib/rainx-keyboard.directive.ts
+++ projects/rainx-keyboard/src/lib/rainx-keyboard.directive.ts
@@ -1,5 +1,5 @@
-import { Directive, ElementRef, HostListener, OnInit } from '@angular/core';
-import { NgControl } from '@angular/forms';
+import { Directive, ElementRef, HostListener, OnInit, Optional } from '@angular/core';
+import { AbstractControl, NgControl } from '@angular/forms';
 import { RainxKeyboardService } from './rainx-keyboard.service';
 
 @Directive({
@@ -11,37 +11,60 @@ export class RainxKeyboardDirective implements OnInit {
   constructor(
     private readonly keyboardService: RainxKeyboardService,
     private readonly elementRef: ElementRef,
-    private readonly ngControl: NgControl) { }
+    @Optional() private readonly ngControl: NgControl) { }
 
   ngOnInit(): void {
     if (this.ngControl) {
       const ctrl = this.ngControl.control;
-      this.keyboardService.keyboardTab.subscribe(() => {
-        if (this.keyboardService.canMove === false) {
-          this.keyboardService.canMove = true;
-          return;
-        }
-        const array = Array.from(document.getElementsByTagName('input')).filter(x => x.hasAttribute('rainxKeyboard'));
-        const index = array.indexOf(document.activeElement as HTMLInputElement);
-        this.keyboardService.canMove = false;
-        setTimeout(() => {
-          array[(index + 1) % array.length].focus();
-        }, 0);
 
-      });
-      this.keyboardService.keyboardClick.subscribe((data: any) => {
-        if (document.activeElement === this.elementRef.nativeElement) {
-          if (ctrl) {
-            ctrl.setValue(data, { emitEvent: false });
-          }
-        }
-      });
+      this.handleTabClick();
+
+      this.handleNonFunctionalKeyboardClick(ctrl);
+
+    } else {
+      this.handleNoBinding();
     }
+  }
+
+  public handleNoBinding(): void {
+    this.keyboardService.keyboardClick.subscribe((data: any) => {
+      if (document.activeElement &&
+        document.activeElement === this.elementRef.nativeElement &&
+        document.activeElement.tagName.toLowerCase() === 'input') {
+        const activeElement = document.activeElement as any;
+        activeElement.value = this.keyboardService.value;
+      }
+    });
+  }
+
+  public handleTabClick(): void {
+    this.keyboardService.keyboardTab.subscribe(() => {
+      if (this.keyboardService.canMove === false) {
+        this.keyboardService.canMove = true;
+        return;
+      }
+      const array = Array.from(document.getElementsByTagName('input')).filter(x => x.hasAttribute('rainxKeyboard'));
+      const index = array.indexOf(document.activeElement as HTMLInputElement);
+      this.keyboardService.canMove = false;
+      setTimeout(() => {
+        array[(index + 1) % array.length].focus();
+      }, 0);
+
+    });
+  }
+
+  public handleNonFunctionalKeyboardClick(ctrl: AbstractControl | null): void {
+    this.keyboardService.keyboardClick.subscribe((data: any) => {
+      if (document.activeElement === this.elementRef.nativeElement) {
+        if (ctrl) {
+          ctrl.setValue(data, { emitEvent: false });
+        }
+      }
+    });
   }
 
   @HostListener('focus', ['$event'])
   public handleFocus(event: any): void {
-    debugger;
     if (!this.keyboardService.isOpen) {
       this.keyboardService.toogleOpen();
     }
