6c4c80150971b10aadde121f5bec2eb456b23d7d
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2021-12-01 20:33:54 +0300

    Libs removed.

--- projects/rainx-keyboard/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of rainx-keyboard
- */
-
-export * from './lib/rainx-keyboard.service';
-export * from './lib/rainx-keyboard.component';
-export * from './lib/rainx-keyboard.module';
--- projects/rainx-upload/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of rainx-upload
- */
-
-export * from './lib/rainx-upload.service';
-export * from './lib/rainx-upload.component';
-export * from './lib/rainx-upload.module';
