c4a5b3723a6301517956ab9215b6d12e12cc4cc1
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2021-12-02 23:19:39 +0300

    File upload method added.

--- projects/rainx-upload/src/lib/rainx-upload.component.ts
+++ projects/rainx-upload/src/lib/rainx-upload.component.ts
@@ -6,16 +6,25 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['rainx-upload.component.css']
 })
 export class RainxUploadComponent implements OnInit {
-
+  @Input() public mimes: string[] = [];
   @Input() public maxSize: number = 40000;
   @Input() public multiple: boolean = false;
   @Input() public files: File[] = [];
-
   @Output() public filesChanged: EventEmitter<File[]> = new EventEmitter<File[]>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public fileUploaded(event: any) {
+    if (event.target.files) {
+      const files: File[] = Array.from(event.target.files);
+      files.forEach((file: File) => this.files.push(file));
+      this.filesChanged.emit(this.files);
+    }
+  }
+
+
 
 }
