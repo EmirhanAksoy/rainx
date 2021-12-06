a7ed825c6d1e2fba85d725a403e8f0788398d166
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2021-12-06 22:58:36 +0300

    Detail box added to remove or download files.

--- projects/rainx-upload/src/lib/rainx-upload.component.ts
+++ projects/rainx-upload/src/lib/rainx-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'rainx-upload',
@@ -19,6 +19,7 @@ export class RainxUploadComponent implements OnInit {
   /**Two-way binding for files. */
   @Input() public files: File[] = [];
   @Output() public filesChange: EventEmitter<File[]> = new EventEmitter<File[]>();
+  public showDetail: boolean = false;
 
   constructor() { }
 
@@ -26,11 +27,11 @@ export class RainxUploadComponent implements OnInit {
   }
 
   /**
-   * File upload event handler
-   * @param event File upload event
+   * File upload event handler.
+   * @param event File upload event.
    * @returns 
    */
-  public handleFileUploaded(event: any) {
+  public handleFileUploaded(event: any): void {
     if (event.target.files) {
       const files: File[] = Array.from(event.target.files);
       if (this.maxSize && files.some(x => x.size > this.maxSize)) {
@@ -43,13 +44,44 @@ export class RainxUploadComponent implements OnInit {
   }
 
   /**
-   * File reset handler
-   * @param event Mouse click event
+   * File reset handler.
+   * @param event Mouse click event.
    */
-  public handleResetClick(event: MouseEvent) {
+  public handleResetClick(event: MouseEvent): void {
     event.stopPropagation();
     this.files = [];
     this.filesChange.emit(this.files);
+  }
+
+  /**
+  * Show detail handler.
+  * @param event Mouse click event.
+  */
+  public handleDetailClick(event: MouseEvent): void {
+    event.stopPropagation();
+    this.showDetail = !this.showDetail;
+  }
+
+  /**
+   * Download file handler.
+   * @param file Selected file object.
+   */
+  public downloadFileHandler(file: File): void {
+    const anchor: HTMLAnchorElement = window.document.createElement('a');
+    anchor.href = window.URL.createObjectURL(new Blob([file], { type: file.type }));
+    anchor.download = file.name;
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+  }
+
+  /**
+   * Remove file handler.
+   * @param index Selected file index in files array.
+   */
+  public removeFileHandler(index: number): void {
+    this.files.splice(index, 1);
+    this.filesChange.emit(this.files)
   }
 
 }
