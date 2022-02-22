478a82da417dd57708ae9406b6521855845b0357
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2022-02-22 15:10:03 +0300

    Listbox component updated with new features

--- projects/rainx-listbox/src/lib/pipes/removed.pipe.ts
+++ projects/rainx-listbox/src/lib/pipes/removed.pipe.ts
@@ -1,11 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'removed'
+  name: 'removed',
+  pure: false
 })
 export class RemovedPipe implements PipeTransform {
 
   transform(data: any[], removed: any[], value: any): any[] {
+    debugger;
     if (data && data.length) {
       if (removed && removed.length) {
         return data.filter(x => !removed.some(z => z === x[value]))
--- projects/rainx-listbox/src/lib/rainx-listbox.component.ts
+++ projects/rainx-listbox/src/lib/rainx-listbox.component.ts
@@ -7,36 +7,41 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class RainxListboxComponent implements OnInit {
   @Input() public data: any[] = [];
-  @Input() public value: string = 'id';
+  @Input() public key: string = 'id';
   @Input() public display: string | string[] = [];
   @Input() public seperator: string = ' - ';
   @Input() public primitive: boolean = false;
-  @Input() public selected: any | null = null;
-  @Input() public removeable: boolean = false;
+  @Input() public selectedKey: any | null = null;
   @Input() public loading: boolean = false;
+  @Input() public removeable: boolean = false;
+  @Input() public resizeable: boolean = false;
   @Output() public selectionChange: EventEmitter<any> = new EventEmitter<any>();
   @Output() public removeChange: EventEmitter<any> = new EventEmitter<any>();
 
   public removedItems: any[] = [];
   public selectedItems: any[] = [];
 
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  public handleRemoveItem(item: any) {
-    const value: any = this.primitive ? item : item[this.value];
-    if (value === this.selected)
-      this.selected = null;
-
-    this.removeChange.emit(value);
+  public handleRemoveItem(item: any): void {
+    debugger;
+    const selectedItem: any = this.primitive ? item : item[this.key];
+    if (selectedItem === this.selectedKey)
+      this.selectedKey = null;
+    this.removedItems.push(selectedItem);
+    this.removeChange.emit(selectedItem);
   }
 
   public handleSelectionChange(item: any): void {
-    this.selected = item[this.value];
-    this.selectionChange.emit(this.primitive ? this.selected : item);
+    if (this.selectedKey === item[this.key]) {
+      this.selectedKey = null;
+      return;
+    }
+    this.selectedKey = item[this.key]
+    this.selectionChange.emit(this.primitive ? this.selectedKey : item);
   }
 
 
