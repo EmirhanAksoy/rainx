adc2498847b815c5895ccffe147086cfc26500e4
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2022-03-11 15:57:27 +0300

    Find pipe updated

--- projects/rainx-pipes/src/lib/pipes/find.pipe.ts
+++ projects/rainx-pipes/src/lib/pipes/find.pipe.ts
@@ -5,11 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FindPipe implements PipeTransform {
 
-  transform(source: any[], property: string, value: string): string {
+  transform(source: any[], property: string, value: string): any | null {
     if (!source || !source.length || !property.length || !value.length)
       return '';
     const selectedItem: any | undefined = source.find(x => x[property] === value);
-    return selectedItem ? selectedItem : '';
+    return selectedItem ? selectedItem : null;
   }
 
 }
